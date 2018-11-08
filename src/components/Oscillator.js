bb964dab1acc5c8e2f181e680353521e26c8ed12
samashtar <sam.ashtar@flatironschool.com>
2018-11-08 16:22:59 -0500

    working piano

--- src/components/Oscillator.js
+++ src/components/Oscillator.js
@@ -1,43 +1,32 @@
-import React from "react";
-
-export default class Oscillator extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentOscillator: []
-    };
-  }
-
-  componentDidMount() {
-    const context = new AudioContext();
-    const oscillator = context.createOscillator();
-
-    //to connect, start and stop(must be disconnected when it stops )
-    oscillator.connect(context.destination);
-    // oscillator.start(0);
-    // oscillator.stop(0);
-    // oscillator.disconnect(0);
-
-    //gives volume to audio  and connects to destination
-    // const gain = context.createGain();
-    // gain.connect(context.destination);
-    // oscillator.start(0);
-    // gain.volume.value = 1;
-    // gain.volume.value = 0; //muted
-    var Note = require("octavian").Note;
-
-    let note1 = new Note("A4");
-    //frequency value
-    oscillator.frequency.value = note1.frequency;
-
-    //type of waves
-    oscillator.type = "sine";
-    this.setState({
-      currentOscillator: oscillator
-    });
-  }
-
-  render() {
-    return null;
-  }
-}
+// import React from "react";
+//
+// export default class Oscillator extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       clicked: false
+//     };
+//   }
+//
+//   changeStateAndHandleClick = () => {
+//     this.setState({
+//       clicked: !this.state.clicked
+//     });
+//     // if (this.state.clicked === true) {
+//     //   this.props.stopKey(this.props.note.id);
+//     // } else {
+//     this.props.playKey(this.props.note.id);
+//     // }
+//   };
+//
+//   render() {
+//     return (
+//       <div
+//         onClick={this.changeStateAndHandleClick}
+//         onKeyDown={this.handleKeyPress}
+//       >
+//         I am Note {this.props.note.id}
+//       </div>
+//     );
+//   }
+// }
--- src/components/PianoContainer.js
+++ src/components/PianoContainer.js
@@ -1,18 +1,56 @@
 import React from "react";
-import Oscillator from "./Oscillator.js";
+import ReactDOM from "react-dom";
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import "react-piano/dist/styles.css";
+import DimensionsProvider from "./DimensionsProvider";
+import SoundfontProvider from "./SoundfontProvider";
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const soundfontHostname = "https://d1pzp51pvbm36p.cloudfront.net";
+
+const noteRange = {
+  first: MidiNumbers.fromNote("c3"),
+  last: MidiNumbers.fromNote("c5")
+};
+const keyboardShortcuts = KeyboardShortcuts.create({
+  firstNote: noteRange.first,
+  lastNote: noteRange.last,
+  keyboardConfig: KeyboardShortcuts.HOME_ROW
+});
 
 export default class PianoContainer extends React.Component {
   constructor() {
     super();
+    this.state = {
+      octave: 4,
+      currentNotes: [],
+      sound: [],
+      currentOscillatorsPlaying: []
+    };
   }
 
-  componentDidMount() {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-  }
-  render() {
+  render(props) {
     return (
       <div>
-        <Oscillator />
+        <DimensionsProvider>
+          {({ containerWidth, containerHeight }) => (
+            <SoundfontProvider
+              instrumentName="acoustic_grand_piano"
+              audioContext={audioContext}
+              hostname={soundfontHostname}
+              render={({ isLoading, playNote, stopNote }) => (
+                <Piano
+                  noteRange={noteRange}
+                  width={containerWidth}
+                  playNote={playNote}
+                  stopNote={stopNote}
+                  disabled={isLoading}
+                  {...props}
+                />
+              )}
+            />
+          )}
+        </DimensionsProvider>
       </div>
     );
   }
