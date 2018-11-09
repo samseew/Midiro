3b2074bafaf25636b484ff1c932d8ab5f420c1cf
samashtar <sam.ashtar@flatironschool.com>
2018-11-09 15:53:05 -0500

    recording skelton

--- src/components/PianoContainer.js
+++ src/components/PianoContainer.js
@@ -1,37 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import { KeyboardShortcuts } from "react-piano";
 import "react-piano/dist/styles.css";
+
 import DimensionsProvider from "./DimensionsProvider";
 import SoundfontProvider from "./SoundfontProvider";
 import { Button, Icon, Dropdown } from "semantic-ui-react";
 import jsonData from "../data.json";
 import "../piano.css";
+import MusicRecorder from "./MusicRecorder.js";
+import _ from "lodash";
+import PianoRecord from "./PianoRecord.js";
 
 const titleize = require("titleize");
 const menuOptions = jsonData.map(el => {
   let sound = titleize(el.split("_").join(" "));
-  let lol = { value: el, text: sound };
-  return lol;
+  let soundEl = { value: el, text: sound };
+  return soundEl;
 });
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const soundfontHostname = "https://d1pzp51pvbm36p.cloudfront.net";
 
-// 18 keys counting first and last
-// const noteRange = {
-//   first: 48,
-//   last: 65
-// };
-// const lowerCaseShortCuts = KeyboardShortcuts.create({
-//   firstNote: 65,
-//   lastNote: 82,
-//   keyboardConfig: KeyboardShortcuts.HOME_ROW
-// });
-//
-// const upperCaseShortcuts = [...lowerCaseShortCuts].map(el => {
-//   return { key: el.key.toUpperCase(), midiNumber: el.midiNumber };
-// });
 export default class PianoContainer extends React.Component {
   constructor() {
     super();
@@ -41,9 +31,81 @@ export default class PianoContainer extends React.Component {
         first: 48,
         last: 65
       },
-      instrumentName: "acoustic_grand_piano"
+      instrumentName: "acoustic_grand_piano",
+      recording: {
+        mode: null,
+        events: [],
+        currentTime: 0,
+        currentEvents: []
+      },
+      recordColor: null
     };
+    this.scheduledEvents = [];
   }
+
+  getRecordingEndTime = () => {
+    if (this.state.recording.events.length === 0) {
+      return 0;
+    }
+    return Math.max(
+      ...this.state.recording.events.map(event => event.time + event.duration)
+    );
+  };
+
+  setRecording = value => {
+    this.setState({
+      recording: Object.assign({}, this.state.recording, value)
+    });
+  };
+
+  onClickPlay = () => {
+    this.setRecording({
+      mode: "PLAYING"
+    });
+
+    const startAndEndTimes = _.uniq(
+      _.flatMap(this.state.recording.events, event => [
+        event.time,
+        event.time + event.duration
+      ])
+    );
+    startAndEndTimes.forEach(time => {
+      this.scheduledEvents.push(
+        setTimeout(() => {
+          const currentEvents = this.state.recording.events.filter(event => {
+            return event.time <= time && event.time + event.duration > time;
+          });
+          this.setRecording({
+            currentEvents
+          });
+        }, time * 1000)
+      );
+    });
+    // Stop at the end
+    setTimeout(() => {
+      this.onClickStop();
+    }, this.getRecordingEndTime() * 1000);
+  };
+
+  onClickStop = () => {
+    this.scheduledEvents.forEach(scheduledEvent => {
+      clearTimeout(scheduledEvent);
+    });
+    this.setRecording({
+      mode: "RECORDING",
+      currentEvents: []
+    });
+  };
+
+  onClickClear = () => {
+    this.onClickStop();
+    this.setRecording({
+      events: [],
+      mode: "RECORDING",
+      currentEvents: [],
+      currentTime: 0
+    });
+  };
 
   keyboardShortcuts() {
     let lowerCaseShortCuts = KeyboardShortcuts.create({
@@ -108,6 +170,20 @@ export default class PianoContainer extends React.Component {
       instrumentName: sound.value
     });
   };
+  startRecording = () => {
+    // let copy = [...this.state.recording];
+    // copy.mode = "RECORDING";
+    console.log("start");
+    if (this.state.recordColor === null) {
+      this.setState({
+        recordColor: "red"
+      });
+    } else {
+      this.setState({
+        recordColor: null
+      });
+    }
+  };
 
   render(props) {
     return (
@@ -133,7 +209,9 @@ export default class PianoContainer extends React.Component {
               audioContext={audioContext}
               hostname={soundfontHostname}
               render={({ isLoading, playNote, stopNote }) => (
-                <Piano
+                <PianoRecord
+                  recording={this.state.recording}
+                  setRecording={this.setRecording}
                   noteRange={this.state.noteRange}
                   width={containerWidth}
                   playNote={playNote}
@@ -146,12 +224,31 @@ export default class PianoContainer extends React.Component {
             />
           )}
         </DimensionsProvider>
-        <Button.Group labeled icon>
-          <Button icon="record" content="record" />
-          <Button icon="stop" content="stop" />
-          <Button icon="play" content="play" />
-        </Button.Group>
+        <div>
+          <MusicRecorder
+            record={this.startRecording.bind(this)}
+            play={this.onClickPlay}
+            stop={this.onClickStop}
+            clear={this.onClickClear}
+            recordColor={this.state.recordColor}
+          />
+        </div>
       </div>
     );
   }
 }
+
+// 18 keys counting first and last
+// const noteRange = {
+//   first: 48,
+//   last: 65
+// };
+// const lowerCaseShortCuts = KeyboardShortcuts.create({
+//   firstNote: 65,
+//   lastNote: 82,
+//   keyboardConfig: KeyboardShortcuts.HOME_ROW
+// });
+//
+// const upperCaseShortcuts = [...lowerCaseShortCuts].map(el => {
+//   return { key: el.key.toUpperCase(), midiNumber: el.midiNumber };
+// });
